2167d44a2fa7fc1e5b811081e9380d0a33f8d148
Viktor Vallmark <vallmark.viktor@gmail.com>
2024-10-02 23:24:48 +0200

    first commit lab10
    Signed-off-by: Viktor Vallmark <vallmark.viktor@gmail.com>

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
   public String getGreeting() {
-    return "Hello World!";
+    return "Welcome to my solutions!";
   }
 
   public static void main(String[] args) {
